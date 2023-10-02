34a2d4ad2ac0f34583d8ded6b4454eaf1f0f9ff7
Sanduranga <edirisinghest527@gmail.com>
2023-10-03 01:28:45 +0530

    made API and test GET & POST requests with fronend

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/firstapi", (request, response) => {
-  response.json({ test: ["one", "two", "three"] });
+  response.json({
+    name: "sanduranga",
+    age: 26,
+    gender: "male",
+    subjects: [
+      { subject1: "maths", marks1: 68 },
+      { subject2: "chemistry", marks2: 80 },
+      { subject3: "physics", marks3: 90 },
+    ],
+  });
+});
+
+app.post("/firstapi", async (request, response) => {
+  try {
+    if (!request.body.name || !request.body.age || !request.body.gender) {
+      console.log("aaaa");
+      response.send("err");
+    } else {
+      console.log("ssss");
+      const newStudent = {
+        name: request.body.name,
+        age: request.body.age,
+        gender: request.body.gender,
+        subject1: request.body.subject1,
+        marks1: request.body.marks1,
+        subject2: request.body.subject2,
+        marks2: request.body.marks2,
+        subject3: request.body.subject3,
+        marks3: request.body.marks3,
+      };
+      return response.json(newStudent);
+    }
+  } catch (error) {
+    console.log(error);
+    response.send(error);
+  }
 });
 
 app.listen(5555, () => {
